53de15625ee2fbc5fc97acd8e2368c059175b433
gdumani <gdumani@gmail.com>
2023-03-25 21:30:43 -0600

    add error handling when api is not available

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -2,22 +2,30 @@ import React, { useEffect, useState } from 'react';
 
 const Quotes = () => {
   const [quote, setQuote] = useState([]);
-  // const [err, setErr] = useState(false);
+  const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch('https://api.api-ninjas.com/v1/quotes?category=learning', {
-      headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': 'KRev7DslVYdLwqvXAAI9aw==vVzIoYVezJ0ueOjR',
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => setQuote(data))
-      .then(() => setLoading(false));
+    const getQuotes = () => {
+      fetch('https://api.api-ninjas.com/v1/quotes?category=learning', {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': 'KRev7DslVYdLwqvXAAI9aw==vVzIoYVezJ0ueOjR',
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setLoading(false);
+          setQuote(data);
+        }, (err) => {
+          setErr(err);
+          setLoading(false);
+        });
+    };
+    getQuotes();
   }, []);
 
   if (loading) return <div> Loading! </div>;
-  // if (err) return <div> No quotes available</div>;
+  if (err) return <div> No quotes available</div>;
   return (
     <div>
       <p className="quote">{quote[0].quote}</p>
