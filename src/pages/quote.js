0071054dfac6ad0ef7ea190cfcc1520a733788d8
gdumani <gdumani@gmail.com>
2023-03-30 12:27:29 -0600

    create styling files and land pages

---
+++ src/pages/quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CurrentQuote from '../components/CurrentQuotes';
+import '../style/quotes.css';
+
+const Quote = () => (
+  <div className="quotes">
+    <CurrentQuote />
+  </div>
+);
+
+export default Quote;
